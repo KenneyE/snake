c12627ba71bf81818e98aead5f903fcaf0f1f8b8
Eric Kenney <ekenney5@gmail.com>
2014-05-11 22:05:23 -0700

    Fixed bug for turning back quickly

--- snake_ui.js
+++ snake_ui.js
@@ -3,11 +3,13 @@
 
     var View = Game.View = function ($el) {
         this.$el = $el;
+                
         this.board = new Game.Board();
         this.setupGame();
     };
 
     View.prototype.setupGame = function () {
+        this.keyQueue = [];
         this.board.setupBoard();
         this.int = undefined;
         this.start();
@@ -16,10 +18,11 @@
     View.prototype.start = function () {
         $(window).on('keydown', this.handleKeyEvent.bind(this));
         this.board.addApple();
-        var gameOver = false; 
         
         this.int = setInterval(function () {
+            if (this.keyQueue.length > 0 ) this.turn();
             this.board.snake.move();
+            
             if ( this.board.snake.isCollided(this.int) ) {this.setupGame();}
            
             this.board.eatApple();
@@ -28,16 +31,25 @@
     };
 
     View.prototype.handleKeyEvent = function (event) {
+
+        var validMove = this.keyQueue.length < 3;
+        validMove = validMove && [38, 39, 40, 37].indexOf(event.keyCode) != -1;
+        validMove = validMove && event.keyCode != this.keyQueue.slice(-1);
+        
+        if ( validMove ) {
+            this.keyQueue.push(event.keyCode);
+        }
+    };
+    
+    View.prototype.turn = function () {
         var keyMap = {
             "38": "N",
             "39": "E",
             "40": "S",
             "37": "W"
         };
-
-        if ([38, 39, 40, 37].indexOf(event.keyCode) != -1) {
-            this.board.snake.turn(keyMap[event.keyCode]);
-        }
+            
+        this.board.snake.turn(keyMap[this.keyQueue.shift()]);
     };
 
     View.prototype.render = function (event) {
